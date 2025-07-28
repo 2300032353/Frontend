53f363b218385db6f5d75465ce561729d05b3dfd
2300032353 <2300032353@kluniversity.in>
2025-07-28 09:54:37 +0530

    First commit

---
+++ Front.js
@@ -0,0 +1,12 @@
+function updateClock() {
+  let now = new Date();
+
+  let hrs = String(now.getHours()).padStart(2, '0');
+  let mins = String(now.getMinutes()).padStart(2, '0');
+  let secs = String(now.getSeconds()).padStart(2, '0');
+
+  console.log(`${hrs}:${mins}:${secs}`);
+}
+setInterval(updateClock, 1000);
+
+updateClock();
